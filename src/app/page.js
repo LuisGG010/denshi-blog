67b753e1734381fdd16bf002d036c5b4dcbb31aa
LuisGG010 <50489293+LuisGG010@users.noreply.github.com>
2026-01-29 13:12:04 -0600

    layout

--- src/app/page.js
+++ src/app/page.js
@@ -2,22 +2,14 @@ import Link from 'next/link';
 
 export default function Home(){
   return (
-    <div className="min-h-screen p-10 bg-black text-white">
+    <div>
       <header>
-        <h1 className="text-4xl front-bold text-blue-500">
+        <h1 className="text-4xl font-bold text-blue-500">
           Denshi blog
         </h1>
         <p className="mt-4 text-xl">
           Bienvenido a mi rincon en internet.
         </p>
-
-        <div className="mt-6">
-          <Link
-            href="/sobre-mi"
-            className='bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition'>
-            Conóceme mejor &rarr;
-          </Link>
-        </div>
       </header>
 
 
--- src/app/sobre-mi/page.js
+++ src/app/sobre-mi/page.js
@@ -3,17 +3,12 @@ import Link from 'next/link';
 export default function SobreMi(){
   return(
     
-    <div className="min-h-screen p-10 bg-gray-900 text-white">
-      <div className="mb-6">
-        <Link href="/" className="text-gray-400 hover:text-white underline">
-          &larr; Volver al inicio
-        </Link>
-      </div>
+    <div>
       <h1 className="text-3xl font-bold text-green-400">
         ¿Quien soy?
       </h1>
       <p className="mt-5 text-lg">
-        Soy un desarrollador aprendiendo Next.js y Verceñ.
+        Soy un desarrollador aprendiendo Next.js y Vercel.
         Este blog es mi proyecto personal para documentar mis cosas.
       </p>
       <div className="mt-8 p-4 bg-gray-800 rounded-lg border border-gray-600">
