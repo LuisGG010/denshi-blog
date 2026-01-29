50ae083c7ab2444f7ebf7b35f4ca4bd98cabf7f2
LuisGG010 <50489293+LuisGG010@users.noreply.github.com>
2026-01-29 12:22:18 -0600

    Agregada pagina sobre mi

---
+++ src/app/sobre-mi/page.js
@@ -0,0 +1,21 @@
+export default function SobreMi(){
+  return(
+    <div className="min-h-screen p-10 bg-gray-900 text-white">
+      <h1 className="texet-3xl font-bold text-green-400">
+        ¿Quien soy?
+      </h1>
+      <p className="mt-5 text-lg">
+        Soy un desarrollador aprendiendo Next.js y Verceñ.
+        Este blog es mi proyecto personal para documentar mis cosas.
+      </p>
+      <div className="mt-8 p-4 bg-gray-800 rounded-lg border border-gray-600">
+        <h3 className="font-bold text-xl mb-2">Mis metas:</h3>
+        <ul classname="list-disc list-inside text-gray-300">
+          <li>Aprender Next.js a fondo</li>
+          <li>Dominar el despliegue de la nube</li>
+          <li>Crear contenido bueno</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
