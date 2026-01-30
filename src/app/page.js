2bea57be8b2af377756c716e52f545d9a08a554e
LuisGG010 <50489293+LuisGG010@users.noreply.github.com>
2026-01-29 18:51:16 -0600

    posts

--- src/app/page.js
+++ src/app/page.js
@@ -1,27 +1,53 @@
+import { supabase } from '@/lib/supabase';
 import Link from 'next/link';
 
-export default function Home(){
+// No almacenar en caché para ver cambios al instante
+export const revalidate = 0; 
+
+export default async function Home(){
+  // Pedimos los datos en la nube
+  const { data: posts, error } = await supabase
+    .from('posts')
+    .select('*')
+    .order('create_at', { ascending: false });
+
+if (error){
+  console.error('Error al obtener los posts:', error);
+}
+
   return (
-    <div>
-      <header>
-        <h1 className="text-4xl font-bold text-blue-500">
-          Denshi blog
-        </h1>
-        <p className="mt-4 text-xl">
-          Bienvenido a mi rincon en internet.
-        </p>
+    <div className='max-w-2xl mx-auto'>
+      <header className='mb-8 text-center'>
+        <h1 className="text-3xl font-bold text-blue-500">Muro Social</h1>
+        <p className="text-gray-400">Lo que voy aprendiendo día a día.</p>
       </header>
 
+      <div className="mb-10 p-4 bg-gray-900 rounded-lg border border-gray-800">
+        <input
+          type="text"
+          placeholder="¿Qué estás pensando?"
+          className='w-full p-3 bg-black border-gray-700 rounded text-white focus:outline-none focus:border-blue-500'
+        />
+      </div>
+      <div className='space-y-4'>
+        {posts?.map((post) => (
+        <article
+          key={post.id}
+          className='p-5 border border-gray-800 rounded-lg bg-gray-900/50 hover:border-blue-500/50 transition'
+          >
+            <p className='text-lg text-gray-200'>{post.content}</p>
+            <p className='text-gray-500 text-xs mt-3 black text-right'>
+              {new Date(post.created_at).toLocaleString()}
+            </p>
+          </article>
+          ))}
 
-
-      <main className="mt-10">
-        <div className="p-6 border border-gray-700 rounded-lg">
-          <h2 className="text2-xl font-semibold">Mi primera entrada</h2>
-          <p className="mt-2 text-gray-300">
-            Hola mundo, estoy aprendiendo a crear esto con Next.js.
-          </p>
-        </div>
-      </main>
+          {posts?.length === 0 && (
+            <p className='text-center text-gray-500 py-10'>
+              Nadie ah escrito nada aún...
+            </p>
+          )}
+      </div>
     </div>
   );
 }
